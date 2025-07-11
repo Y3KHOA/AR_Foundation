1f27da9149ba4ef9e104bd72f3c712a3629f4f8b
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-07-11 10:43:26 +0700

    Fix layout for button text

---
+++ Assets/Editor/MenuButtonUIEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MenuButtonUI))]
+public class MenuButtonUIEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Preview Data"))
+        {
+            var targetScript = target as MenuButtonUI;
+            MethodInfo method = typeof(MenuButtonUI).GetMethod("PreviewData", BindingFlags.Instance | BindingFlags.NonPublic);
+            
+            if (method != null)
+            {
+                method.Invoke(targetScript, null);
+            }
+            else
+            {
+                Debug.LogWarning("Method not found!");
+            }
+        }
+    }
+}
--- Assets/Scripts/MenuButtonUI.cs
+++ Assets/Scripts/MenuButtonUI.cs
@@ -1,16 +1,42 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class MenuButtonUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TextMeshProUGUI titleText;
+    [SerializeField] private TextMeshProUGUI descriptionText;
+    [SerializeField] private ButtonTextData buttonTextData;
+
+    private void LoadData()
     {
+        if (buttonTextData == null)
+        {
+            Debug.LogError("Kiểm tra lại xem đã có button text data chưa",gameObject);
+            return;
+        }
         
+        titleText.text = buttonTextData.titleTextData.data;
+        descriptionText.text = buttonTextData.descriptionTextData.data;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PreviewData()
     {
-        
+        LoadData();
+    }
+    
+}
+[CreateAssetMenu(fileName = "Button_Default",menuName = "SO/Button Text Data")]
+public class ButtonTextData : ScriptableObject
+{
+
+    public TextData titleTextData;
+    public TextData descriptionTextData;
+    
+    [Serializable]
+    public struct TextData
+    {
+        public string languageTag;
+        public string data;
     }
 }
