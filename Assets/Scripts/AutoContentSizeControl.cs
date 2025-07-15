221779e2544d7b5594fb5ea62c0bf791e43db667
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-07-15 16:24:33 +0700

    Chỉnh các button nằm giữa layout

--- Assets/Scripts/AutoContentSizeControl.cs
+++ Assets/Scripts/AutoContentSizeControl.cs
@@ -8,4 +8,35 @@ public class AutoContentSizeControl : MonoBehaviour
     {
         rectTransform = GetComponent<RectTransform>();
     }
+
+    private float totalWidth, totalHeigh;
+    private int previousChildCount = -1;
+
+    private void LateUpdate()
+    {
+        int childCount = 0;
+        foreach (Transform item in rectTransform.transform)
+        {
+            if(item.gameObject.activeSelf)
+                childCount += 1;
+        }
+
+        if (childCount != previousChildCount)
+        {
+            totalWidth = 0;
+            totalHeigh = 0;
+            foreach (RectTransform item in rectTransform.transform)
+            {
+                Debug.Log("Plus: " + item.rect.width);
+                totalWidth += item.rect.width;
+                totalWidth += item.rect.height;
+            }
+
+            Debug.Log($"$Current Size Delta {rectTransform.rect.width}");
+            Debug.Log($"Total width {totalWidth}");
+            Debug.Log($"Total heigh {totalHeigh}");
+            previousChildCount = childCount;
+            rectTransform.sizeDelta = new Vector2(totalWidth / 1.7f, rectTransform.sizeDelta.y);
+        }
+    }
 }
---
+++ Assets/Scripts/LimitScrollView.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LimitScrollView : MonoBehaviour
+{
+    [SerializeField] private ScrollRect scrollRect;
+
+    private void Awake()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+    }
+
+    private void Update()
+    {
+        scrollRect.verticalNormalizedPosition = Mathf.Clamp(scrollRect.verticalNormalizedPosition, 0f, 1f);
+    }
+}
