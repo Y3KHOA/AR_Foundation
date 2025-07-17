8170fbe2e28863fbdb603dbdb16de75f4378014d
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-07-17 15:07:16 +0700

    Thêm toggle cho cơ chế vẽ

---
+++ Assets/Scripts/UI/ToggleButtonLineType.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+
+public class ToggleButtonLineType : ToggleButtonColorUI
+{
+    public LineType lineType;
+    private TextMeshProUGUI textFunc;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        textFunc = GetComponentInChildren<TextMeshProUGUI>();
+        string text = null;
+        switch (lineType)
+        {
+            case LineType.Door:
+                text = "Cửa";
+                break;
+            case LineType.Wall:
+                text = "Tường";
+                break;
+            case LineType.Window:
+                text = "Cửa sổ";
+                break;
+        }
+        textFunc.text = text;
+    }
+}
