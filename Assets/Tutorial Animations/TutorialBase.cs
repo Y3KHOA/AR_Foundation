748edb2f21358baa51ee8dd66f1e4971ff869f3a
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-07-17 11:04:27 +0700

    Add resize icon

---
+++ Assets/Tutorial Animations/TutorialBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class TutorialBase : MonoBehaviour
+{
+    protected bool isPlayAgain = false;
+
+    protected virtual void Awake()
+    {
+        TutorialHandle.tutorial = this;
+    }
+
+
+    public abstract void StopTutorial();
+
+    public abstract void PlayTutorial();
+
+
+}
