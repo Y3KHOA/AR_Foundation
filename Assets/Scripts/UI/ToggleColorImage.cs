e9b16dd510c5c7c24eab10bca85f3bdfa9cc228f
Nguyễn Việt Quang <quangviet932@gmail.com>
2025-07-28 16:36:16 +0700

    Chỉnh sửa UI theo feedback

--- Assets/Scripts/UI/ToggleColorImage.cs
+++ Assets/Scripts/UI/ToggleColorImage.cs
@@ -4,7 +4,10 @@ using UnityEngine.UI;
 public class ToggleColorImage : ToggleColorBase
 {
     [SerializeField] private Image toggleColorImage;
-
+    // for prototype
+    [SerializeField] private Image toggleSprite;
+    [SerializeField] private Sprite deActiveSprite;
+    [SerializeField] private Sprite activeSprite;
     public override void Toggle(bool isActive)
     {
         if (toggleColorImage == null)
@@ -13,6 +16,13 @@ public class ToggleColorImage : ToggleColorBase
             return;
         }
         toggleColorImage.color = isActive ? activeColor : deActiveColor;
+
+        if (!toggleSprite|| !activeSprite || !deActiveSprite)
+        {
+            return;
+        }
+        
+        toggleSprite.sprite = isActive ? activeSprite : deActiveSprite;
     }
 
     
